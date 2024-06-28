a8411cde31f76e13954145350deeef8c94b6bd2e
RAMOHAM <reda.ali.moham@gmail.com>
2024-06-28 21:04:00 +0100

    Car Item Card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import {CarSale} from "@/components/CarSale";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CarSale />
+        <CarSale name={"2015 - Toyota"} price={"$5000.00"} />
+        <CarSale name={"2018 - Honda"} price={"$3700.00"} />
     </main>
   );
 }
--- src/components/CarSale.tsx
+++ src/components/CarSale.tsx
@@ -1,22 +1,46 @@
-import {Card, CardBody, CardFooter} from "@nextui-org/react";
-import Image from "next/image";
+"use client"
 
-export const CarSale = () => {
+import {Button, Card, CardBody, CardFooter} from "@nextui-org/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import {useState} from "react";
+
+type CarItemData = {
+    name: string,
+    price: string
+}
+
+export const CarSale = (carItem: CarItemData) => {
+    const [isFavourite, setIsFavourite] = useState(false)
     return (
         <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-            <Card shadow="sm" isPressable>
-                <CardBody className="overflow-visible p-0">
+            <Card className="w-80" shadow="sm" isPressable={false}>
+                <CardBody className="relative overflow-visible h-48 rounded-2xl">
                     <Image
                         alt="title"
-                        className="w-full object-cover h-[140px]"
+                        className="w-full object-cover h-full rounded-t-lg"
                         src="/toyota.png"
                         width={100}
-                        height={25}
+                        height={50}
                     />
+                    <div className="absolute text-center rounded-full bg-white border-black border-2 top-0 right-0 w-8 h-8 mt-[-8px] mr-[-8px]"
+                         onClick={() => {
+                             setIsFavourite(!isFavourite)
+                         }}
+                    >
+                        <FontAwesomeIcon
+                            icon={faHeart}
+                            className={`${isFavourite ? 'text-sm text-red-600' : 'text-sm text-black'}`}
+                        />
+                    </div>
                 </CardBody>
-                <CardFooter className="text-small justify-between">
-                    <b>Toyota</b>
-                    <p className="text-default-500">$5000.00</p>
+                <CardFooter className="flex flex-col text-small bg-white p-4 rounded-b-lg">
+                    <div className="flex justify-between w-full">
+                        <b className="text-blue-600 font-bold">{carItem.name}</b>
+                        <p className="text-blue-600 font-bold">{carItem.price}</p>
+                    </div>
+                    <Button className="self-end bg-blue-600 text-white p-2 mt-2 w-20 rounded-2xl">Buy</Button>
                 </CardFooter>
             </Card>
         </div>
