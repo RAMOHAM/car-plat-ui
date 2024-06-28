bf5282f25407c908836567e7024d68708c799bee
RAMOHAM <reda.ali.moham@gmail.com>
2024-06-28 21:42:17 +0100

    Navbar heading added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import {CarSale} from "@/components/CarSale";
+import {NavbarComponent} from "@/components/NavbarComponent";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <NavbarComponent></NavbarComponent>
         <CarSale name={"2015 - Toyota"} price={"$5000.00"} />
         <CarSale name={"2018 - Honda"} price={"$3700.00"} />
     </main>
---
+++ src/components/NavbarComponent.tsx
@@ -0,0 +1,37 @@
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+
+export const NavbarComponent = () => {
+    return(
+        <>
+            <Navbar className="bg-gray-800 text-white py-4 fixed top-0 left-0 w-full z-50" shouldHideOnScroll>
+                <NavbarBrand>
+                    <p className="font-bold text-inherit ml-5">MOTOR APPS</p>
+                </NavbarBrand>
+                <NavbarContent className="hidden sm:flex gap-8" justify="center">
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            HOME
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem isActive>
+                        <Link href="#" aria-current="page">
+                            MANUFACTURERS
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link color="foreground" href="#">
+                            CONTACT US
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    <NavbarItem>
+                        <Button as={Link} className="mr-5 bg-blue-600 pl-5 pr-5 pt-2 pb-2 rounded-2xl" href="#" variant="flat">
+                            Login
+                        </Button>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
+        </>
+    )
+}
